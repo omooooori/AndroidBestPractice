1b16d1ec2f8d819122273b73b9ee1d370819cfc5
omooooori <r.shimizu18020@gmail.com>
2021-10-31 10:18:40 +0900

    Android test practice Data Bindingの有効化。

--- Android_Test_Practice/app/src/main/java/com/example/android_test_practice/MainActivity.kt
+++ Android_Test_Practice/app/src/main/java/com/example/android_test_practice/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.android_test_practice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.android_test_practice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(
+            this,
+            R.layout.activity_main
+        )
+
+        binding.textView.text = "Hello Binding"
     }
 }
